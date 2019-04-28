09bf102c50086dbb807f5d43726fd0acab5e39ae
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-28 15:31:07 +0300

    Created ConstantsUtil class

---
+++ src/main/java/com/qthegamep/gnikcom_bot/util/ConstantsUtil.java
@@ -0,0 +1,13 @@
+package com.qthegamep.gnikcom_bot.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ConstantsUtil {
+
+    public final String LOG_PATH = "log.path";
+    public final String CONFIG_PATH = "config.path";
+    public final String TELEGRAM_BOT_NAME = "telegram.bot.name";
+    public final String TELEGRAM_BOT_USERNAME = "telegram.bot.username";
+    public final String TELEGRAM_BOT_TOKEN = "telegram.bot.token";
+}
