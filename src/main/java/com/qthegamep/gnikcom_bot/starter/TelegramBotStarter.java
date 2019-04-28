a650140705b0119ff6aea33f9614e6c785817857
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-28 15:07:39 +0300

    Created TelegramBotStarter class

---
+++ src/main/java/com/qthegamep/gnikcom_bot/starter/TelegramBotStarter.java
@@ -0,0 +1,39 @@
+package com.qthegamep.gnikcom_bot.starter;
+
+import com.qthegamep.gnikcom_bot.controller.MainControllerImpl;
+import com.qthegamep.gnikcom_bot.util.LogUtil;
+
+import lombok.experimental.UtilityClass;
+import lombok.val;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+@UtilityClass
+class TelegramBotStarter {
+
+    void start() throws TelegramApiRequestException {
+        initBot();
+        logSuccessStart();
+    }
+
+    private void initBot() throws TelegramApiRequestException {
+        ApiContextInitializer.init();
+        val telegramBotsApi = createTelegramBotsApi();
+        val bot = createBot();
+        telegramBotsApi.registerBot(bot);
+    }
+
+    private TelegramBotsApi createTelegramBotsApi() {
+        return new TelegramBotsApi();
+    }
+
+    private MainControllerImpl createBot() {
+        return new MainControllerImpl();
+    }
+
+    private void logSuccessStart() {
+        LogUtil.logInfo("Telegram bot \"{}\" was started!", System.getProperty("telegram.bot.name"));
+    }
+}
