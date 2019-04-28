5e169396ca23aa7d753968db7b89c8717e7f37f1
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-28 15:10:51 +0300

    Added telegram bot starter

--- src/main/java/com/qthegamep/gnikcom_bot/Application.java
+++ src/main/java/com/qthegamep/gnikcom_bot/Application.java
@@ -3,9 +3,11 @@ package com.qthegamep.gnikcom_bot;
 import com.qthegamep.gnikcom_bot.exception.CouldNotStartApplicationException;
 import com.qthegamep.gnikcom_bot.starter.ApplicationStarter;
 
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 public class Application {
 
-    public static void main(String[] args) throws CouldNotStartApplicationException {
+    public static void main(String[] args) throws CouldNotStartApplicationException, TelegramApiRequestException {
         ApplicationStarter.startApplication();
     }
 }
--- src/main/java/com/qthegamep/gnikcom_bot/starter/ApplicationStarter.java
+++ src/main/java/com/qthegamep/gnikcom_bot/starter/ApplicationStarter.java
@@ -5,14 +5,21 @@ import com.qthegamep.gnikcom_bot.exception.CouldNotStartApplicationException;
 
 import lombok.experimental.UtilityClass;
 
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 @UtilityClass
 public class ApplicationStarter {
 
-    public void startApplication() throws CouldNotStartApplicationException {
+    public void startApplication() throws CouldNotStartApplicationException, TelegramApiRequestException {
         initConfig();
+        startTelegramBot();
     }
 
     private void initConfig() throws CouldNotStartApplicationException {
         Config.getInstance();
+    }
+
+    private void startTelegramBot() throws TelegramApiRequestException {
+        TelegramBotStarter.start();
     }
 }
