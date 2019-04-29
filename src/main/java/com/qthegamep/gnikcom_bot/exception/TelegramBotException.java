86fa28f4be351d2f52d697d870ce3aaad506994a
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-29 14:09:46 +0300

    Updated TelegramBotException class: - Added new constructor;

--- src/main/java/com/qthegamep/gnikcom_bot/exception/TelegramBotException.java
+++ src/main/java/com/qthegamep/gnikcom_bot/exception/TelegramBotException.java
@@ -5,4 +5,8 @@ public class TelegramBotException extends RuntimeException {
     public TelegramBotException(String message) {
         super(message);
     }
+
+    public TelegramBotException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
