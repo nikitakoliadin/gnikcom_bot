d565d2af166f7daece0104ebe0612e9b224044f3
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-29 17:50:40 +0300

    Created CommandFactory class;

---
+++ src/main/java/com/qthegamep/gnikcom_bot/factory/CommandFactory.java
@@ -0,0 +1,27 @@
+package com.qthegamep.gnikcom_bot.factory;
+
+import com.qthegamep.gnikcom_bot.command.*;
+import com.qthegamep.gnikcom_bot.util.CommandsUtil;
+
+public class CommandFactory {
+
+    public Command createCommand(String command) {
+        switch (command) {
+            case CommandsUtil.INVITE_SYSTEM_COMMAND: {
+                return new InviteCommand();
+            }
+            case CommandsUtil.REMOVE_SYSTEM_COMMAND: {
+                return new RemoveCommand();
+            }
+            case CommandsUtil.START_COMMAND: {
+                return new StartCommand();
+            }
+            case CommandsUtil.HELP_COMMAND: {
+                return new HelpCommand();
+            }
+            default: {
+                return new DefaultCommand();
+            }
+        }
+    }
+}
