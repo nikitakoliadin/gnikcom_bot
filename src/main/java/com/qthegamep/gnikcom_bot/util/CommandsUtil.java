01a5377361726e8aec66974a0dbbc5efbd8e5cf1
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-29 16:37:05 +0300

    Created CommandsUtil class

---
+++ src/main/java/com/qthegamep/gnikcom_bot/util/CommandsUtil.java
@@ -0,0 +1,13 @@
+package com.qthegamep.gnikcom_bot.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CommandsUtil {
+
+    public final String INVITE_SYSTEM_COMMAND = "INVITE";
+    public final String REMOVE_SYSTEM_COMMAND = "REMOVE";
+    public final String DEFAULT_COMMAND = "DEFAULT";
+    public final String START_COMMAND = "/start";
+    public final String HELP_COMMAND = "/help";
+}
