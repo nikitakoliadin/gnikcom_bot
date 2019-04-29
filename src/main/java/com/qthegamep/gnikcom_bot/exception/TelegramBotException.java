caa73aaca46a55b70eaede39ffe2331d921d0fef
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-29 12:38:08 +0300

    Created TelegramBotException class

---
+++ src/main/java/com/qthegamep/gnikcom_bot/exception/TelegramBotException.java
@@ -0,0 +1,8 @@
+package com.qthegamep.gnikcom_bot.exception;
+
+public class TelegramBotException extends RuntimeException {
+
+    public TelegramBotException(String message) {
+        super(message);
+    }
+}
