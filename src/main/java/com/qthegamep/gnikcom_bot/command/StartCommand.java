fad88b36186679aa44878f26f5ba4f8e3011fd95
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-30 15:35:03 +0300

    Implemented StartCommand class

--- src/main/java/com/qthegamep/gnikcom_bot/command/StartCommand.java
+++ src/main/java/com/qthegamep/gnikcom_bot/command/StartCommand.java
@@ -1,13 +1,46 @@
 package com.qthegamep.gnikcom_bot.command;
 
+import lombok.val;
+
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class StartCommand implements Command {
 
+    private SendMessage response = new SendMessage();
+
     @Override
     public SendMessage buildResponse(Update update) {
-        // TODO: Implements
-        return null;
+        val message = update.getMessage();
+        setFlags();
+        setChatId(message);
+        setText(message);
+        return response;
+    }
+
+    private void setFlags() {
+        response.enableMarkdown(true);
+        response.enableNotification();
+    }
+
+    private void setChatId(Message message) {
+        val chatId = message.getChatId();
+        response.setChatId(chatId);
+    }
+
+    private void setText(Message message) {
+        val text = getText(message);
+        response.setText(text);
+    }
+
+    private String getText(Message message) {
+        if (message.isUserMessage()) {
+            // TODO: Implements
+            return "_Test user start command_";
+        } else {
+            // TODO: Implements
+            return "_Test group start command_";
+        }
     }
 }
