23b8db8c21b1561340531e8f9a1c8ad4fbf466b3
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-30 17:54:26 +0300

    Implemented DefaultCommand class

--- src/main/java/com/qthegamep/gnikcom_bot/command/DefaultCommand.java
+++ src/main/java/com/qthegamep/gnikcom_bot/command/DefaultCommand.java
@@ -1,13 +1,41 @@
 package com.qthegamep.gnikcom_bot.command;
 
+import lombok.val;
+
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class DefaultCommand implements Command {
 
+    private SendMessage response = new SendMessage();
+
     @Override
     public SendMessage buildResponse(Update update) {
+        val message = update.getMessage();
+        setFlags();
+        setChatId(message);
+        setText();
+        return response;
+    }
+
+    private void setFlags() {
+        response.enableMarkdown(true);
+        response.enableNotification();
+    }
+
+    private void setChatId(Message message) {
+        val chatId = message.getChatId();
+        response.setChatId(chatId);
+    }
+
+    private void setText() {
+        val text = getText();
+        response.setText(text);
+    }
+
+    private String getText() {
         // TODO: Implements
-        return null;
+        return "_Test default command_";
     }
 }
