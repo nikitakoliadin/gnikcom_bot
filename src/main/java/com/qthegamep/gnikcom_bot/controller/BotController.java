75aa5fa09627db1ac5e821d6d5609a74222a4fca
Nikita Koliadin <qThegamEp@gmail.com>
2019-05-01 11:17:47 +0300

    Updated BotController class: - Refactored class; - Added handling of CommandShouldNotBeExecutedException;

--- src/main/java/com/qthegamep/gnikcom_bot/controller/BotController.java
+++ src/main/java/com/qthegamep/gnikcom_bot/controller/BotController.java
@@ -1,6 +1,7 @@
 package com.qthegamep.gnikcom_bot.controller;
 
 import com.qthegamep.gnikcom_bot.command.Command;
+import com.qthegamep.gnikcom_bot.exception.CommandShouldNotBeExecutedException;
 import com.qthegamep.gnikcom_bot.exception.TelegramBotException;
 import com.qthegamep.gnikcom_bot.factory.CommandFactory;
 import com.qthegamep.gnikcom_bot.service.CommandService;
@@ -29,12 +30,19 @@ public class BotController extends TelegramLongPollingBot {
     }
 
     @Override
-    public void onUpdateReceived(Update update) throws TelegramBotException {
-        logRequestInfo(update);
-        val textCommand = getTextCommand(update);
-        val command = getCommand(textCommand);
-        val response = command.execute(update);
-        sendResponse(response);
+    public void onUpdateReceived(Update update) throws TelegramBotException, CommandShouldNotBeExecutedException {
+        try {
+            logRequestInfo(update);
+            val textCommand = getTextCommand(update);
+            val command = getCommand(textCommand);
+            val response = command.execute(update);
+            sendResponse(response);
+        } catch (CommandShouldNotBeExecutedException e) {
+            LogUtil.logError("{}", e.getMessage());
+        } catch (TelegramApiException e) {
+            LogUtil.logError("ERROR", e);
+            throw new TelegramBotException(e.getMessage(), e);
+        }
     }
 
     private void logRequestInfo(Update update) {
@@ -51,13 +59,8 @@ public class BotController extends TelegramLongPollingBot {
         return commandFactory.createCommand(textCommand);
     }
 
-    private void sendResponse(BotApiMethod<? extends Serializable> response) throws TelegramBotException {
-        try {
-            val responseResult = execute(response);
-            LogUtil.logInfo("Response: {}", responseResult);
-        } catch (TelegramApiException e) {
-            LogUtil.logError("ERROR", e);
-            throw new TelegramBotException(e.getMessage(), e);
-        }
+    private void sendResponse(BotApiMethod<? extends Serializable> response) throws TelegramApiException {
+        val responseResult = execute(response);
+        LogUtil.logInfo("Response: {}", responseResult);
     }
 }
