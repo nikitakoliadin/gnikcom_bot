15b5808559bc8ffad38cdc09746e3b8556baed7c
Nikita Koliadin <qThegamEp@gmail.com>
2019-05-09 12:48:23 +0300

    Updated ApplicationUtil class: - Added COMMAND_TEXTS_PATH const;

--- src/main/java/com/qthegamep/gnikcom_bot/util/ApplicationUtil.java
+++ src/main/java/com/qthegamep/gnikcom_bot/util/ApplicationUtil.java
@@ -7,6 +7,7 @@ public class ApplicationUtil {
 
     public final String LOG_PATH = "log.path";
     public final String CONFIG_PATH = "config.path";
+    public final String COMMAND_TEXTS_PATH = "command.texts";
     public final String TELEGRAM_BOT_NAME = "telegram.bot.name";
     public final String TELEGRAM_BOT_USERNAME = "telegram.bot.username";
     public final String TELEGRAM_BOT_TOKEN = "telegram.bot.token";
