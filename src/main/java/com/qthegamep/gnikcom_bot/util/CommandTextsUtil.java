79764dd247fb24d1535a46fb27b42ee7166e16ca
Nikita Koliadin <qThegamEp@gmail.com>
2019-05-09 13:53:16 +0300

    Updated CommandTextsUtil class: - Implements text filling; - Refactored;

--- src/main/java/com/qthegamep/gnikcom_bot/util/CommandTextsUtil.java
+++ src/main/java/com/qthegamep/gnikcom_bot/util/CommandTextsUtil.java
@@ -10,55 +10,31 @@ import java.util.Random;
 @UtilityClass
 public class CommandTextsUtil {
 
-    // TODO: Implements
-    public final List<? extends String> START_USER_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test user start command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> START_GROUP_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test group start command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> HELP_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test help command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> INVITE_BOT_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test bot invite command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> INVITE_USER_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test user invite command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> INVITE_USERS_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test users invite command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> REMOVE_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test remove command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> DEFAULT_NOT_FOUND_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test not found default command");
-    }};
-
-    // TODO: Implements
-    public final List<? extends String> DEFAULT_CREATE_GROUP_COMMAND_TEXTS = new ArrayList<>() {{
-        add("Test create group default command");
-    }};
+    public final List<? extends String> START_USER_COMMAND_TEXTS = fillTexts("start.user.command");
+    public final List<? extends String> START_GROUP_COMMAND_TEXTS = fillTexts("start.group.command");
+    public final List<? extends String> HELP_COMMAND_TEXTS = fillTexts("help.command");
+    public final List<? extends String> INVITE_BOT_COMMAND_TEXTS = fillTexts("invite.bot.command");
+    public final List<? extends String> INVITE_USER_COMMAND_TEXTS = fillTexts("invite.user.command");
+    public final List<? extends String> INVITE_USERS_COMMAND_TEXTS = fillTexts("invite.users.command");
+    public final List<? extends String> REMOVE_COMMAND_TEXTS = fillTexts("remove.command");
+    public final List<? extends String> DEFAULT_NOT_FOUND_COMMAND_TEXTS = fillTexts("default.not.found.command");
+    public final List<? extends String> DEFAULT_CREATE_GROUP_COMMAND_TEXTS = fillTexts("default.create.group.command");
 
     public String getRandomTextFrom(List<? extends String> texts) {
         val size = texts.size();
         val index = getIndex(size);
         return texts.get(index);
+    }
+
+    private List<? extends String> fillTexts(String prefix) {
+        val texts = new ArrayList<String>();
+        val properties = System.getProperties();
+        properties.forEach((key, value) -> {
+            if (key.toString().startsWith(prefix)) {
+                texts.add((String) value);
+            }
+        });
+        return texts;
     }
 
     private int getIndex(int size) {
