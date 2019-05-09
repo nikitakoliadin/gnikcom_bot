2a8dbdcddd663ce94209e03d3d90bf8a7d11e3c8
Nikita Koliadin <qThegamEp@gmail.com>
2019-05-09 14:20:27 +0300

    Updated ConfigUtil class: - Added ApplicationUtil dependency; - Updated CONFIG_PATH path; - Added LOG_PATH path; - Added COMMAND_TEXTS_PATH path;

--- src/test/java/com/qthegamep/gnikcom_bot/testhelper/util/ConfigUtil.java
+++ src/test/java/com/qthegamep/gnikcom_bot/testhelper/util/ConfigUtil.java
@@ -1,11 +1,15 @@
 package com.qthegamep.gnikcom_bot.testhelper.util;
 
+import com.qthegamep.gnikcom_bot.util.ApplicationUtil;
+
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ConfigUtil {
 
     public void setPathToTestProperties() {
-        System.setProperty("config.path", "src/main/resources/properties/config.properties");
+        System.setProperty(ApplicationUtil.LOG_PATH, "C:/opt/logs/gnikcom_bot");
+        System.setProperty(ApplicationUtil.CONFIG_PATH, "src/main/resources/properties/config.properties");
+        System.setProperty(ApplicationUtil.COMMAND_TEXTS_PATH, "src/main/resources/texts/commandTexts.properties");
     }
 }
